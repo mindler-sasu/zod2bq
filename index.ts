a4f747f5552e75b250aabd6d96502b4a51d97224
mindler-sasu <sasu.makinen@mindler.se>
2022-03-10 10:01:33 +0200

    npm

--- index.ts
+++ index.ts
@@ -1,18 +1,2 @@
 import { inferBQ } from "./src/zod2bq";
-import { z } from "zod";
-
-const stringSchema = z.object({ bar: z.string(), beer: z.string() });
-const bq = inferBQ(stringSchema);
-console.log(bq);
-// [
-//   { name: 'bar', type: 'STRING', mode: 'NULLABLE' },
-//   { name: 'beer', type: 'STRING', mode: 'NULLABLE' }
-// ]
-
-const stringSchema2 = z.object({ bar: z.string(), beer: z.string() }).partial();
-const bq2 = inferBQ(stringSchema2);
-console.log(bq2);
-// [
-//   { name: 'bar', type: 'STRING', mode: 'NULLABLE' },
-//   { name: 'beer', type: 'STRING', mode: 'NULLABLE' }
-// ]
+export const zod2bq = inferBQ;
